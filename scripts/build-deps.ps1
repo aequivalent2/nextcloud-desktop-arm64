e9e9d459304d7b6dabb0c036ea78597f459b55bb
Leonhard Ruckert <119690710+aequivalent2@users.noreply.github.com>
2026-06-02 23:13:55 +0200

    fix: Qt-Pfade 6.11.1->6.9.0 arm64_cross_compiled, vcpkg-Clone entfernt (wird im Workflow gebootstrapped)

--- scripts/build-deps.ps1
+++ scripts/build-deps.ps1
@@ -1,6 +1,8 @@
 Write-Host "Building dependencies for ARM64..."
 
-$env:PATH = "C:\Qt\6.11.1\msvc2022_arm64\bin;C:\Qt\Tools\CMake\bin;C:\Qt\Tools\Ninja;" + $env:PATH
+$QtDir = "C:\Qt\6.9.0\msvc2022_arm64_cross_compiled"
+
+$env:PATH = "$QtDir\bin;C:\Qt\Tools\CMake\bin;C:\Qt\Tools\Ninja;" + $env:PATH
 $env:PATH = ($env:PATH -split ';' | Where-Object { $_ -notlike '*Strawberry*' }) -join ';'
 $env:LIB = "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.26100.0\um\arm64;" +
            "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.26100.0\ucrt\arm64;" +
@@ -24,27 +26,42 @@ cmake --install .
 # QtKeychain
 git clone https://github.com/frankosterfeld/qtkeychain.git C:\build\qtkeychain-src
 mkdir C:\build\qtkeychain-build; Set-Location C:\build\qtkeychain-build
-& "C:\Qt\6.11.1\msvc2022_arm64\bin\qt-cmake.bat" C:\build\qtkeychain-src -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=C:\build\deps-arm64\qtkeychain -DOPENSSL_ROOT_DIR=C:\build\openssl-arm64
+& "$QtDir\bin\qt-cmake.bat" C:\build\qtkeychain-src `
+  -G Ninja `
+  -DCMAKE_BUILD_TYPE=Release `
+  -DCMAKE_INSTALL_PREFIX=C:\build\deps-arm64\qtkeychain `
+  -DOPENSSL_ROOT_DIR=C:\build\openssl-arm64
 cmake --build . --parallel
 cmake --install . --config Release --prefix C:\build\deps-arm64\qtkeychain
 
 # KF6Archive
+# vcpkg wurde bereits im Workflow gebootstrapped (C:\build\vcpkg)
 git clone https://invent.kde.org/frameworks/karchive.git C:\build\karchive-src
 mkdir C:\build\karchive-build; Set-Location C:\build\karchive-build
 
-# vcpkg for sqlite3 and compression libs
-git clone https://github.com/microsoft/vcpkg.git C:\build\vcpkg
-C:\build\vcpkg\bootstrap-vcpkg.bat
-C:\build\vcpkg\vcpkg.exe install sqlite3:arm64-windows zlib:arm64-windows bzip2:arm64-windows liblzma:arm64-windows zstd:arm64-windows
+# Compression-Libs via vorhandenes vcpkg nachinstallieren
+C:\build\vcpkg\vcpkg.exe install zlib:arm64-windows bzip2:arm64-windows liblzma:arm64-windows zstd:arm64-windows --no-print-usage
 
-& "C:\Qt\6.11.1\msvc2022_arm64\bin\qt-cmake.bat" C:\build\karchive-src -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=C:\build\deps-arm64\kf6archive -DBUILD_TESTING=OFF "-DECM_DIR=C:\build\deps-arm64\ecm\share\ECM\cmake" "-DCMAKE_TOOLCHAIN_FILE=C:\build\vcpkg\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=arm64-windows -DOPENSSL_ROOT_DIR=C:\build\openssl-arm64
+& "$QtDir\bin\qt-cmake.bat" C:\build\karchive-src `
+  -G Ninja `
+  -DCMAKE_BUILD_TYPE=Release `
+  -DCMAKE_INSTALL_PREFIX=C:\build\deps-arm64\kf6archive `
+  -DBUILD_TESTING=OFF `
+  "-DECM_DIR=C:\build\deps-arm64\ecm\share\ECM\cmake" `
+  "-DCMAKE_TOOLCHAIN_FILE=C:\build\vcpkg\scripts\buildsystems\vcpkg.cmake" `
+  -DVCPKG_TARGET_TRIPLET=arm64-windows `
+  -DOPENSSL_ROOT_DIR=C:\build\openssl-arm64
 cmake --build . --parallel
 cmake --install . --prefix C:\build\deps-arm64\kf6archive
 
 # KDSingleApplication
 git clone https://github.com/KDAB/KDSingleApplication.git C:\build\kdsingleapp-src
 mkdir C:\build\kdsingleapp-build; Set-Location C:\build\kdsingleapp-build
-& "C:\Qt\6.11.1\msvc2022_arm64\bin\qt-cmake.bat" C:\build\kdsingleapp-src -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=C:\build\deps-arm64\kdsingleapp -DKDSingleApplication_QT6=ON
+& "$QtDir\bin\qt-cmake.bat" C:\build\kdsingleapp-src `
+  -G Ninja `
+  -DCMAKE_BUILD_TYPE=Release `
+  -DCMAKE_INSTALL_PREFIX=C:\build\deps-arm64\kdsingleapp `
+  -DKDSingleApplication_QT6=ON
 cmake --build . --parallel
 cmake --install . --config Release --prefix C:\build\deps-arm64\kdsingleapp
 
