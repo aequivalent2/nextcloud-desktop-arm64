db4ae724d449d071c8df1171d78946da8b8a4ad0
Leo Pan <leonhard.ruckert@outlook.com>
2026-07-12 13:41:40 +0200

    CHange

--- scripts/build-libp11.ps1
+++ scripts/build-libp11.ps1
@@ -11,6 +11,7 @@ cl.exe /nologo /W3 /MT /O2 /D_WIN32_WINNT=0x0600 /DWIN32_LEAN_AND_MEAN `
     /DWIN32 /D_WINDOWS /I"C:\build\openssl-arm64\include" /I"." `
     /c eng_back.c eng_err.c eng_front.c libpkcs11.c p11_atfork.c `
        p11_attr.c p11_cert.c p11_ckr.c p11_ec.c p11_eddsa.c p11_err.c `
+       p11_falcon.c p11_mldsa.c p11_slhdsa.c `
        p11_front.c p11_key.c p11_load.c p11_misc.c p11_pkey.c `
        p11_rsa.c p11_slot.c util_uri.c
 
@@ -47,6 +48,7 @@ link.exe /DLL /NOLOGO /MACHINE:ARM64 `
     /DEF:libp11_all.def *.obj `
     C:\build\openssl-arm64\lib\libcrypto.lib `
     ws2_32.lib user32.lib advapi32.lib crypt32.lib
+if ($LASTEXITCODE -ne 0) { throw "libp11-Linking fehlgeschlagen (Exit $LASTEXITCODE)" }
 
 # libp11.lib liegt bereits in C:\build\libp11-arm64, p11.lib als Alias
 Copy-Item C:\build\libp11-arm64\libp11.lib C:\build\libp11-arm64\p11.lib
